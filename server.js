9e734ae588684b0907f87d036e2004285e5744fc
rdna9565428771@gmail.com <rdna9565428771@gmail.com>
2025-09-04 16:03:31 +0530

    fix: allowed all cors origin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors({ origin: "*" }));
 
 // Routes
 app.use("/", searchRoutes);
